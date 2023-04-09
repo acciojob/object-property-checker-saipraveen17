4ebc3744e1b51567c51cc226282fcc1627dc6778
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-09 12:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject[key]==undefined) return false;
+	else return true;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+const key = prompt("Enter Key."); 
 alert(hasKey(key));
